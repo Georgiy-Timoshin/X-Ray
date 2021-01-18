ea42379e407e7a0d5912334d6fc4df5f08b910ac
xrModder <53347567+xrModder@users.noreply.github.com>
2021-01-19 00:27:48 +0600

    Тень от актёра

--- src/xrEngine/xrGame/HUDManager.cpp
+++ src/xrEngine/xrGame/HUDManager.cpp
@@ -197,6 +197,20 @@ void CHUDManager::Render_Last()
 	O->OnHUDDraw					(this);
 	::Render->set_HUD				(FALSE);
 }
+
+void CHUDManager::Render_Actor_Shadow()
+{
+	if (0==pUI)						return;
+	CObject*	O					= g_pGameLevel->CurrentViewEntity();
+	if (0==O)						return;
+	CActor*		A					= smart_cast<CActor*> (O);
+	if (!A)							return;
+	if (A->active_cam() != eacFirstEye) return;
+
+	::Render->set_Object			(O->H_Root());
+	O->renderable_Render			();
+}
+
 extern void draw_wnds_rects();
 extern ENGINE_API BOOL bShowPauseString;
 //отрисовка элементов интерфейса
