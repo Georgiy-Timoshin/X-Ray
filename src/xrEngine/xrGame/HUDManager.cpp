67fd5454a22e48b4e371ec49cd69a7b190e8e193
xrModder <53347567+xrModder@users.noreply.github.com>
2021-01-19 01:15:07 +0600

    Обновить миникарту после изменения разрешения экрана

--- src/xrEngine/xrGame/HUDManager.cpp
+++ src/xrEngine/xrGame/HUDManager.cpp
@@ -285,6 +285,8 @@ void CHUDManager::OnScreenRatioChanged()
 	pUI->UIMainIngameWnd->Init			();
 	pUI->UnLoad							();
 	pUI->Load							(pUI->UIGame());
+	if (b_online)
+		pUI->OnConnected();
 }
 
 void CHUDManager::OnDisconnected()
